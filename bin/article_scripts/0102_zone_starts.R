9a1d0f0fc71e0715cb207fd41bac6eb191cb6767
Christian <Christiancal.lee@mail.utoronto.ca>
2021-01-06 21:23:49 -0800

    zone starts final

--- bin/article_scripts/0102_zone_starts.R
+++ bin/article_scripts/0102_zone_starts.R
@@ -71,13 +71,16 @@ gc()
 
 
 
-## --------------------
+## -------------------------------------------------------------------------------------------------
 ## ---- analyze ---- ##
 library(ggplot2)
 library(ggrepel)
 library(RColorBrewer)
 library(formattable)
 library(ggpubr)
+library(data.table)
+
+setwd("~/Documents/hockey-stats/data/")
 
 print(load("~/Documents/hockey-stats/data/1231_team_game_pdo_2018-2019.rsav"))
 dim(df2)
@@ -131,7 +134,7 @@ ggplot(res, aes(x=gp, y=avg_pdo, group=team)) +
 
 
 
-## -------------------------------
+## ---------------------------------------
 ## ---- player summary zone starts ---- ##
 ## load adv stats from: https://www.hockey-reference.com/leagues/NHL_2020_skaters-advanced.html
 df = read.csv("~/Documents/hockey-stats/data/adv_hockey_stats.csv", header=T, skip=1, stringsAsFactors=F)
@@ -149,7 +152,6 @@ df = df[df$GP >= 20,]
 ## get groups
 mean_value = mean(df$oZS.)
 sd_value = sd(df$oZS.)
-df$group = ifelse(df$oZS. > mean_value + 2*sd_value | df$oZS. < mean_value - 2*sd_value, "extreme")
 
 
 ## plot
@@ -182,8 +184,11 @@ formattable(df_tab,
 
 ## combine with points
 bstats = read.csv("basic_stats_2020.csv", header=T, skip=1, stringsAsFactors=F)
-bstats = bstats[,c(2,7:10,22)]
-colnames(bstats) = c("name","goals","assists", "points","pm","toi")
+bstats$ev_points = bstats[,13] + bstats[,17]
+bstats = bstats[,c(2,7:10,22,29)]
+colnames(bstats) = c("name","goals","assists", "points","pm","toi","ev_points")
+bstats = bstats[order(bstats$points, decreasing = T),]
+bstats = bstats[!duplicated(bstats$name),]
 bstats$name = gsub("\\\\.*","",bstats$name)
 df2 = merge(df, bstats, by="name")
 
@@ -192,12 +197,13 @@ df2 = merge(df, bstats, by="name")
 ## scatter plot against points
 df2 = df2[order(df2$oZS., decreasing = T),]
 df2$ppg = df2$points / df2$GP
-names_to_label = c(df2$name[1:10], "Leon Draisaitl", "Connor McDavid", "Artemi Panarin")
+dff = df2[grepl("W|C|F",df2$Pos),]
+names_to_label = c(dff$name[1:10], "Leon Draisaitl", "Connor McDavid", "Artemi Panarin")
 
-ggplot(df2, aes(x=oZS., y=ppg)) +
+ggplot(dff, aes(x=oZS., y=ppg)) +
   geom_point(size=1.25, color="gray30", alpha=.7) +
   geom_smooth() +
-  geom_label_repel(data=df2[df2$name %in% names_to_label,],aes(x=oZS., y=ppg, label=name),
+  geom_label_repel(data=dff[dff$name %in% names_to_label,],aes(x=oZS., y=ppg, label=name),
                    size=3.5, alpha=1, force = 15,
                    # ylim=c(NA,2e6),
                    #xlim=c(65,NA),
@@ -206,14 +212,177 @@ ggplot(df2, aes(x=oZS., y=ppg)) +
                    show.legend  = F) +
 
   labs(title="Season average offensize zone starts",
-       subtitle="2019-2020 regular season\nn = 790 skaters",
+       subtitle=paste0("2019-2020 regular season\nn = ", nrow(dff)," forwards"),
        x="oZS%", y="Points per game") +
   stat_cor(method="spearman", alternative = "greater", size=5.5, show.legend = F) + 
   #scale_y_continuous(breaks=seq(10,90, 20), limits=c(0, 100)) +
-  scale_x_continuous(limits=c(18, 72), breaks=seq(20,70,10)) +
+  scale_x_continuous(limits=c(20, 65), breaks=seq(20,70,10)) +
   theme_bw() +
   theme(axis.text=element_text(size=12),
         title=element_text(size=13),
         legend.text=element_text(size=11))
+
+
+## same plot with even strength points
+df2 = df2[order(df2$oZS., decreasing = T),]
+df2$ev_ppg = df2$ev_points / df2$GP
+dff = df2[grepl("W|C|F",df2$Pos),]
+names_to_label = c(dff$name[1:10], "Leon Draisaitl", "Connor McDavid", "Artemi Panarin")
+ggplot(dff, aes(x=oZS., y=ev_ppg)) +
+  geom_point(size=1.25, color="gray30", alpha=.7) +
+  geom_smooth() +
+  geom_label_repel(data=dff[dff$name %in% names_to_label,],aes(x=oZS., y=ev_ppg, label=name),
+                   size=3.5, alpha=1, force = 15,
+                   # ylim=c(NA,2e6),
+                   #xlim=c(65,NA),
+                   segment.size  = 0.2,
+                   min.segment.length = 0,
+                   show.legend  = F) +
+  
+  labs(title="Season average offensize zone starts",
+       subtitle=paste0("2019-2020 regular season\nn = ", nrow(dff)," forwards"),
+       x="oZS%", y="EV points per game") +
+  stat_cor(method="spearman", alternative = "greater", size=5.5, show.legend = F) + 
+  #scale_y_continuous(breaks=seq(10,90, 20), limits=c(0, 100)) +
+  scale_x_continuous(limits=c(20, 65), breaks=seq(20,70,10)) +
+  theme_bw() +
+  theme(axis.text=element_text(size=12),
+        title=element_text(size=13),
+        legend.text=element_text(size=11))
+
+
+## ---- DEFENSEMEN ---- ##
+dfd = df2[!grepl("W|C",df2$Pos),]
+names_to_label = c(dfd$name[1:10])
+
+ggplot(dfd, aes(x=oZS., y=ppg)) +
+  geom_point(size=1.25, color="gray30", alpha=.7) +
+  geom_smooth() +
+  geom_label_repel(data=dfd[dfd$name %in% names_to_label,],aes(x=oZS., y=ppg, label=name),
+                   size=3.5, alpha=1, force = 15,
+                   # ylim=c(NA,2e6),
+                   #xlim=c(65,NA),
+                   segment.size  = 0.2,
+                   min.segment.length = 0,
+                   show.legend  = F) +
+  
+  labs(title="Season average offensize zone starts",
+       subtitle=paste0("2019-2020 regular season\nn = ", nrow(dfd), " defensemen"),
+       x="oZS%", y="Points per game") +
+  stat_cor(method="spearman", alternative = "greater", size=5.5, show.legend = F) + 
+  #scale_y_continuous(breaks=seq(10,90, 20), limits=c(0, 100)) +
+  scale_x_continuous(limits=c(30, 70), breaks=seq(20,70,10)) +
+  theme_bw() +
+  theme(axis.text=element_text(size=12),
+        title=element_text(size=13),
+        legend.text=element_text(size=11))
+
+
+
+
+
+
+
+## -------------------------
+## ---- team summary ---- ##
+team_df = fread("team_summary_18-19.csv")
+summary_df = fread("faceoff_percentages_team_summary_18-19.csv")
+df3 = merge(team_df, summary_df, by="Team")
+
+df3$`OZ FO` = gsub(",","",df3$`OZ FO`)
+df3$`DZ FO` = gsub(",","",df3$`DZ FO`)
+df3$oZS = as.numeric(df3$`OZ FO`) / as.numeric(as.numeric(df3$`OZ FO`) + as.numeric(df3$`DZ FO`))
+
+
+ggplot(df3, aes(x=oZS, y=P, label=Team)) +
+  geom_point(size=2, color="gray25", alpha=1) +
+  geom_smooth() +
+  geom_label_repel(alpha=.85, size=3.75) + 
+  labs(title="Season average offensize zone starts",
+       subtitle="2018-2019 regular season",
+       x="oZS%", y="Points") +
+  stat_cor(method="spearman", alternative = "greater", size=5.5, show.legend = F) + 
+  #scale_y_continuous(breaks=seq(10,90, 20), limits=c(0, 100)) +
+  #scale_x_continuous(limits=c(18, 72), breaks=seq(20,70,10)) +
+  theme_bw() +
+  theme(axis.text=element_text(size=12),
+        title=element_text(size=13),
+        legend.text=element_text(size=11))
+
+
+
+
+
+
+## -------------------------
+## ---- game-by-game ---- ##
+## get df of counts
+print(load("0102_team_game_zone_starts_2018-2019.rsav"))
+colnames(df2)[3] = "game"
+df2$oZS = as.numeric(df2$`OZ FO`) / as.numeric(as.numeric(df2$`OZ FO`) + as.numeric(df2$`DZ FO`))
+
+## get basic game stats also sorted game
+print(load("1215_nhl_home_summary_stats_2018-2019.rsav"))
+basic_df = df_cleaned[,c("game", "wins", "losses", "points", "goal_for","goal_against")]
+
+
+## combine and add CF% (cfp)
+df = merge(df2, basic_df, by="game")
+df$oZS = df$oZS * 100
+
+dodge = position_dodge(width = .89)
+ggplot(df, aes(x = points, y = oZS)) +
+  geom_violin(position = dodge, alpha=.35, fill="dodgerblue4", color="dodgerblue4") +
+  geom_boxplot(position = dodge, alpha=.67, fill="#396591", outlier.shape = NA, width=.5) + 
+  geom_hline(yintercept = 50, linetype = "dashed", color = "red", alpha = .5, size=1) +
+  scale_fill_manual(values=c("CF%" = "#185d9e", "SOG%" = "#6a9ccc")) + 
+  scale_color_manual(values=c("CF%" = "#185d9e", "SOG%" = "#6a9ccc")) + 
+  labs(title = "Distribution of oZS% by points", subtitle = "2018-2019 full regular season\nn = 1271 home games",
+       x="Points", y="oZS%",
+       fill="Statistic",color="Statistic") +
+  #stat_compare_means() +
+  theme_bw() +
+  theme(axis.text=element_text(size=12),
+        title=element_text(size=13),
+        legend.text=element_text(size=11))
+
+
+
+
+## by bins
+## x = 15 bins
+## y = win percentage
+df$bin = ntile(df$oZS, 5)
+x = df[,c("bin","wins","oZS","Team")]
+y = setDT(x)[, c("win_percentage", "group_mean") := list(mean(as.numeric(wins)), mean(as.numeric(oZS))), by=list(bin, Team)]
+#y = setDT(x)[, .("win_percentage"=mean(as.numeric(wins)), "group_mean"=mean(as.numeric(oZS))), by="bin"]
+
+
+ggplot(y, aes(group_mean, win_percentage)) +
+  geom_point(size=2) +
+  labs(title = "Mean win percetanges by oZS%", 
+       subtitle = "2018-2019 full regular season\nbins = 15",
+       x="Mean bin oZS%", y="Win percentage") +
+  theme_bw() +
+  theme(axis.text=element_text(size=12),
+        title=element_text(size=13),
+        legend.text=element_text(size=11))
+
+
+## facet line
+y$win_percentage = y$win_percentage * 100
+ggplot(y, aes(group_mean, win_percentage)) +
+  geom_count() + 
+  geom_line() +
+  geom_hline(yintercept = c(75, 50, 25), color="gray25", linetype="dashed", alpha=.5) +
+  scale_x_continuous(breaks=seq(30,70,10), limits=c(30,71)) + 
+  scale_y_continuous(breaks=seq(0,100,25), limits=c(-5,105)) + 
+  labs(x="Bin mean oZS%", y="Win percentage") + 
+  facet_wrap(~Team) +
+  theme_bw() +
+  theme(axis.text=element_text(size=11),
+        axis.title=element_text(size=12),
+        strip.text=element_text(size=10))
+
 
 

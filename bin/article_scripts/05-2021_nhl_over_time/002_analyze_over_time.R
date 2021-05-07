a6f47b510001b939a97ae41561d78d025bf1caf9
Christian <Christiancal.lee@mail.utoronto.ca>
2021-05-07 13:20:42 -0700

    add text labels and colored points

--- bin/article_scripts/05-2021_nhl_over_time/002_analyze_over_time.R
+++ bin/article_scripts/05-2021_nhl_over_time/002_analyze_over_time.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(data.table)
 library(RColorBrewer)
 library(cowplot)
+library(ggrepel)
 
 setwd("~/Documents/hockey-stats/data/")
 
@@ -50,8 +51,9 @@ xbreaks = seq(1, length(unique(df$Season)), 2)
 xlabels = unique(df$Season)[c(TRUE, FALSE)]
  
 p1 = ggplot(df, aes(x=season_num, y=GF/GP)) +
-      geom_point(aes(colour=season_colour), size=1.75) +
-      geom_smooth(alpha=.55, se=F) +
+      geom_point(aes(colour=season_colour), size=1.75, alpha=.9) +
+      geom_point(data=df2[df2$Team == "Edmonton Oilers",], color="black", size=2) +
+      geom_smooth(alpha=.6, se=T) +
       labs(title = "Goals For / Games Played Across NHL Seasons",
            x = "Season", y="GF/GP") +
       scale_colour_identity() +
@@ -64,6 +66,7 @@ p1 = ggplot(df, aes(x=season_num, y=GF/GP)) +
             axis.text.x = element_text(size=13, angle=90, hjust=.5, vjust=.5),
             legend.position="none")
 
+p1
 png("figures/0504_gf_over_gp.png", width=900, height=425)
   p1
 dev.off()
@@ -102,7 +105,7 @@ dev.off()
 df2 = df[!is.na(df$`Shots/GP`),,drop=F]
 p3 = ggplot(df2, aes(x=season_num, y=`Shots/GP`)) +
       geom_point(aes(colour=season_colour), size=1.75) +
-      geom_smooth(alpha=.55, se=F) +
+      geom_smooth(alpha=.6, se=T) +
       labs(title = "Shots on Goal Across NHL Seasons",
            x = "Season", y="SOG") +
       scale_colour_identity() +
@@ -129,10 +132,15 @@ df2$save_percentage = (1 - (df2$`GF/GP` / df2$`Shots/GP`)) * 100
   
 p4 = ggplot(df2, aes(x=season_num, y=save_percentage)) +
       geom_point(aes(colour=season_colour), size=1.75) +
-      geom_smooth(alpha=.55, se=F) +
+      geom_label_repel(data=df2[df2$save_percentage < 84.25,], aes(label=Team),
+                       ylim=c(82,85), alpha=.8, force=10) +
+      geom_label_repel(data=df2[df2$save_percentage > 92.75,], aes(label=Team),
+                       ylim=c(93,95), alpha=.8, force=10) +
+      geom_smooth(alpha=.6, se=T) +
       labs(title = "Save % Across NHL Seasons",
            x = "Season", y="Sv%") +
       scale_colour_identity() +
+      scale_y_continuous(limits=c(83,95)) +
       scale_x_continuous(breaks = xbreaks,
                          labels = xlabels,
                          expand = c(.01,0)) +
@@ -142,7 +150,6 @@ p4 = ggplot(df2, aes(x=season_num, y=save_percentage)) +
             axis.text.x = element_text(size=13, angle=90, hjust=.5, vjust=.5),
             legend.position="none")
 
-p4
 png("figures/0504_save_percentage_over_gp.png", width=900, height=425)
   p4
 dev.off()
@@ -157,7 +164,9 @@ df2 = df[!is.na(df$`PP%`),,drop=F]
 
 p5 = ggplot(df2, aes(x=season_num, y=`PP%`)) +
   geom_point(aes(colour=season_colour), size=1.75) +
-  geom_smooth(alpha=.55, se=F) +
+  geom_smooth(alpha=.6, se=T) +
+  geom_label_repel(data=df2[df2$`PP%` < 10 | df2$`PP%` > 29,], aes(label=Team),
+                   alpha=.8) +
   labs(title = "Power play % Across NHL Seasons",
        x = "Season", y="PP%") +
   scale_colour_identity() +

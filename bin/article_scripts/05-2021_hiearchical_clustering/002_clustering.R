c58bc011df944b4c5b8a8f5141d9d1c49ee3ae26
Christian <Christiancal.lee@mail.utoronto.ca>
2021-05-21 09:30:06 -0700

    create clustering for 2020-2021

--- bin/article_scripts/05-2021_hiearchical_clustering/002_clustering.R
+++ bin/article_scripts/05-2021_hiearchical_clustering/002_clustering.R
@@ -54,7 +54,7 @@ dim(df)
 ## ------------------------------------------------------
 ## All seasons (1917-present)
 
-## keep a max of 500 players for viz purposes
+## keep a max num of players for viz purposes
 df2 = df[,c("G_GP", "A_GP")]
 df2 = df2[1:100,]
 
@@ -149,42 +149,39 @@ rm(df3)
 
 
 ## load data
-print(load("052021_skater_data_reg_season_1917_2021.rsav"))
+print(load("052021_skater_data_reg_season_2020_2021.rsav"))
 dim(df)
-#[1] 7400   22
+#[1] 1000   22
 
 head(df)
 
-
 ## subset and make numeric
-df = df[,c(1,4:8)]
+df = df[,c(1,5,6:10,14,16,18,21)]
 
 ## make columns of interest numeric
-df$GP = as.numeric(gsub(",", "", df$GP))
-df$G = as.numeric(gsub(",", "", df$G))
-df$A = as.numeric(gsub(",", "", df$A))
-df$P = as.numeric(gsub(",", "", df$P))
+df[3:ncol(df)] = sapply(df[3:ncol(df)], as.numeric)
 df$plus_minus = as.numeric(gsub("+|-", "", df$`+/-`))
 df$`+/-` = NULL
 
 
 ## remove any rows missing date and players with < 50 games
-df = df[nchar(df$Player) > 3 & df$GP >= 50,,drop=F]
+df = df[nchar(df$Player) > 3 & df$GP >= 25,,drop=F]
+
 
 ## get stats per GP
-df[,3:6] = df[,3:6] / df[,2]
-colnames(df)[3:6] = paste0(colnames(df)[3:6], "_GP")
+df[,4:ncol(df)] = df[,4:ncol(df)] / df[,3]
+colnames(df)[4:ncol(df)] = paste0(colnames(df)[4:ncol(df)], "_GP")
 df = df[order(df$P_GP, decreasing = T),,drop=F]
 #df[,3:6] = scale(df[,3:6])
 
 
 ## final prep
-df$GP = NULL
-df = df[!duplicated(df$Player),,drop=F]
-rownames(df) = df$Player
-df$Player = NULL
-dim(df)
-#[1] 4605    4
+df2 = df[-3]
+df2 = df2[!duplicated(df2$Player),,drop=F]
+rownames(df2) = df2$Player
+df2$Player = NULL
+dim(df2)
+#[1] 607   9
 
 
 
@@ -192,38 +189,56 @@ dim(df)
 
 ## ------------------------------------------------------
 ## All seasons (1917-present)
+set.seed(8797)
 
-## keep a max of 500 players for viz purposes
-df2 = df[,c("G_GP", "A_GP")]
+## keep a max num of players for viz purposes
+column_ha = columnAnnotation(Pos = df2$Pos[1:100], annotation_name_gp = gpar(fontsize = 8))
+df2$Pos = NULL
 df2 = df2[1:100,]
 
+## scale
+df2 = scale(df2)
 
 
+## ---- agglomerative
+## get distances
+d = dist(df2, method = "euclidean")
 
-
-
-## visualizing clusters in 2D
-#hc5 = cutree(hc1, 5)
-
-# # Visualising the clusters
-# fviz_cluster(list(data = df2, cluster = hc5),
-#              labelsize = 10, geom="point") +
-#     geom_text_repel(data=df2[1:10,], aes(x=G_GP, A_GP), label=rownames(df2)[1:10])
-# 
-
+## hierarchical clustering
+hc1 = hclust(d, method = "ward.D" )
 
 
 
 df3 = as.matrix(t(df2))
 Heatmap(df3, name = "Metric / GP", row_dend_side = "right", column_dend_side = "bottom",
         column_names_gp = gpar(fontsize = 8), row_names_gp = gpar(fontsize = 8),
-        column_dend_height = unit(6, "cm"),
+        column_dend_height = unit(4, "cm"),
         clustering_method_rows = "ward.D",
         clustering_method_columns = "ward.D",
-        column_title = "Hiearchical clustering of the top 100 NHL skaters by Points/GP\n1917-2021 Regular Seasons")
+        bottom_annotation = column_ha,
+        column_title = "Hiearchical clustering of the top 100 NHL skaters by Points/GP\n2020-2021 Regular Seasons")
 
 
 
+
+## ---- kmeans
+set.seed(1234)
+wcss = vector()
+for (i in 1:15) wcss[i] = sum(kmeans(df2, i)$withinss)
+plot(1:15,
+     wcss,
+     type = 'b',
+     main = paste('The Elbow Method'),
+     xlab = 'Number of clusters',
+     ylab = 'WCSS')
+
+
+## visualizing clusters in 2D
+hc6 = cutree(hc1, 6)
+
+# Visualising the clusters
+fviz_cluster(list(data = df2, cluster = hc6),
+            labelsize = 10, geom="point")
 
 
 

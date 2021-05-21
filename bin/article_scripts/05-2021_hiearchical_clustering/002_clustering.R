83d592e40beddf277b2c7f0118128943e81bb38e
Christian <Christiancal.lee@mail.utoronto.ca>
2021-05-20 23:37:23 -0700

    prep 2020-2021 analysis for scrape

--- bin/article_scripts/05-2021_hiearchical_clustering/002_clustering.R
+++ bin/article_scripts/05-2021_hiearchical_clustering/002_clustering.R
@@ -49,6 +49,8 @@ dim(df)
 
 
 
+
+
 ## ------------------------------------------------------
 ## All seasons (1917-present)
 
@@ -94,22 +96,20 @@ hc1 = hclust(d, method = "ward.D" )
 ## plot dendogram
 plot(hc1, cex = 0.6, hang = -1, main = "Dendrogram of NHL skaters")
 
-
-
-## comparing different dendrogram methods on a subset
+## --
+## aside: comparing different dendrogram methods on a subset
 d2 = dist(df2[1:50,], method = "euclidean")
 test1 = hclust(d2, method = "ward.D") 
 test2 = hclust(d2, method = "complete") #complete is maximum difference between ALL pairwise points of cluster
 
-# Create two dendrograms
+## create two dendrograms
 dend1 = as.dendrogram(test1)
 dend2 = as.dendrogram(test2)
-
 dend_list = dendlist(dend1, dend2)
 
 tanglegram(dend1, dend2,
-           common_subtrees_color_lines = FALSE, # Turn-off line colors
-           common_subtrees_color_branches = FALSE, # Color common branches
+           common_subtrees_color_lines = FALSE, 
+           common_subtrees_color_branches = FALSE,
            highlight_distinct_edges = TRUE, 
            highlight_branches_lwd = FALSE,
            columns_width = c(5, 1.5, 5),
@@ -118,7 +118,7 @@ tanglegram(dend1, dend2,
            lwd = 2,
            lab.cex = 1,
            margin_inner = 7)
-
+## --
 
 
 
@@ -143,9 +143,59 @@ Heatmap(df3, name = "G|A / GP", row_dend_side = "right", column_dend_side = "bot
 ## 2020-2021 only
 ## question: are there "tiers" of players based on Goals, Assists, Plus/Minus
 ## or will we just discover positions? (positions akin to molecular subtype)
+rm(df)
+rm(df2)
+rm(df3)
+
+
+## load data
+print(load("052021_skater_data_reg_season_1917_2021.rsav"))
+dim(df)
+#[1] 7400   22
+
+head(df)
+
+
+## subset and make numeric
+df = df[,c(1,4:8)]
+
+## make columns of interest numeric
+df$GP = as.numeric(gsub(",", "", df$GP))
+df$G = as.numeric(gsub(",", "", df$G))
+df$A = as.numeric(gsub(",", "", df$A))
+df$P = as.numeric(gsub(",", "", df$P))
+df$plus_minus = as.numeric(gsub("+|-", "", df$`+/-`))
+df$`+/-` = NULL
+
+
+## remove any rows missing date and players with < 50 games
+df = df[nchar(df$Player) > 3 & df$GP >= 50,,drop=F]
+
+## get stats per GP
+df[,3:6] = df[,3:6] / df[,2]
+colnames(df)[3:6] = paste0(colnames(df)[3:6], "_GP")
+df = df[order(df$P_GP, decreasing = T),,drop=F]
+#df[,3:6] = scale(df[,3:6])
+
+
+## final prep
+df$GP = NULL
+df = df[!duplicated(df$Player),,drop=F]
+rownames(df) = df$Player
+df$Player = NULL
+dim(df)
+#[1] 4605    4
 
 
 
+
+
+## ------------------------------------------------------
+## All seasons (1917-present)
+
+## keep a max of 500 players for viz purposes
+df2 = df[,c("G_GP", "A_GP")]
+df2 = df2[1:100,]
 
 
 
@@ -160,6 +210,19 @@ Heatmap(df3, name = "G|A / GP", row_dend_side = "right", column_dend_side = "bot
 #              labelsize = 10, geom="point") +
 #     geom_text_repel(data=df2[1:10,], aes(x=G_GP, A_GP), label=rownames(df2)[1:10])
 # 
+
+
+
+
+df3 = as.matrix(t(df2))
+Heatmap(df3, name = "Metric / GP", row_dend_side = "right", column_dend_side = "bottom",
+        column_names_gp = gpar(fontsize = 8), row_names_gp = gpar(fontsize = 8),
+        column_dend_height = unit(6, "cm"),
+        clustering_method_rows = "ward.D",
+        clustering_method_columns = "ward.D",
+        column_title = "Hiearchical clustering of the top 100 NHL skaters by Points/GP\n1917-2021 Regular Seasons")
+
+
 
 
 

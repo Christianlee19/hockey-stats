95934caad456dd5b3854a24930dcd9fb03aa40b9
Christian Lee <christianlee@Christians-MacBook-Pro-2.local>
2021-05-20 17:54:53 -0700

    updated scrape

--- bin/article_scripts/05-2021_hiearchical_clustering/001_scrape_master.R
+++ bin/article_scripts/05-2021_hiearchical_clustering/001_scrape_master.R
@@ -9,7 +9,8 @@ library(dplyr)
 
 
 ## set and navigate to url
-url = "http://www.nhl.com/stats/teams?aggregate=0&reportType=season&seasonFrom=19171918&seasonTo=20202021&gameType=2&filter=gamesPlayed,gte,1&sort=a_seasonId&page=0&pageSize=100"
+url = "http://www.nhl.com/stats/skaters?reportType=season&seasonFrom=19171918&seasonTo=20202021&gameType=2&filter=gamesPlayed,gte,1&sort=points,goals,assists&page=0&pageSize=100"
+
 # rD = rsDriver(port=4444L, browser="firefox", version="4.0.0-alpha-1",chromever="90.0.4430.24", check=TRUE) #chromever can change depending on chrome version and updates (see below)
 # remDr = rD[['client']]
 # remDr$navigate(url) 
@@ -20,37 +21,30 @@ remDr$navigate(url)
 src = remDr$getPageSource()[[1]] 
 
 
- 
-## if line 12 failed, try to see versions available:
-# binman::list_versions("seleniumserver")
-# binman::list_versions("chromedriver") #try different chromever starting with those closest to your chrome version
-
-
-
 ## read in the info from the first table
 df = read_html(src) %>% 
   xml_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "rt-td", " " ))]') %>%
   xml_text() %>%
-  matrix(.,ncol=24, byrow = T) %>%
+  matrix(.,ncol=23, byrow = T) %>%
   data.frame(.,stringsAsFactors = F)
 
 
 ## At our url, there are 26 pages if viewing 50 rows at a time.
 ## We are starting at 2 because 1 was already done before.
 ## We will be adding to that data frame so we don't have to define an empty one.
-for (i in 2:16) {
+for (i in 2:74) {
   print(i)
   pages = remDr$findElement(using = "css selector",".-next") #we are selecting the next button
   pages$clickElement()  
   
-  ## wait 5seconds to load (sometimes NHL.com is slow)
-  Sys.sleep(5)
+  ## wait 4 seconds to load (sometimes NHL.com is slow)
+  Sys.sleep(4)
   
   src = remDr$getPageSource()[[1]]
   temp = read_html(src) %>% 
     xml_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "rt-td", " " ))]') %>%
     xml_text() %>%
-    matrix(., ncol=24, byrow = T) %>%
+    matrix(., ncol=23, byrow = T) %>%
     data.frame(., stringsAsFactors = F)
   
   ## bind new data
@@ -69,27 +63,16 @@ colnames(df) = header
 ## remove index column
 df = df[,-1]
 dim(df)
-#[1] 1600   23
+#[1] 7400   22
 
 
 ## save file
-save(df, file = "~/Documents/hockey-stats/data/052021_skater_data_reg_season_2020_21.rsav")
+save(df, file = "~/Documents/hockey-stats/data/052021_skater_data_reg_season_1917_2021.rsav")
 
 ## close
 remDr$close()
 rD$server$stop() 
 gc()
-
-
-
-
-
-
-
-
-## ---- scrape player data from as early as possible
-
-
 
 
 
